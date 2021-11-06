9ca9815fd662be291cdbbf6df18c5c9438e6bf61
Yan Pisani <54196956+yanpisani@users.noreply.github.com>
2021-11-06 16:59:56 -0300

    Teste commit e sidebar constructor deletado

--- frontend/src/components/admin/dashboard/sidebar/index.jsx
+++ frontend/src/components/admin/dashboard/sidebar/index.jsx
@@ -1,11 +1,7 @@
 import React, {Component} from 'react';
 import './styles.scss'
 
-export default class Sidebar extends Component {
-    constructor(props) {
-        super(props);
-    }
-    
+export default class Sidebar extends Component {    
     render() {
         return(
             <div className={`sidebar`}>teste</div>
