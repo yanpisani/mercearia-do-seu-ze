b52a1fa46c0fe6d6338e0d0d3e28a0a94c9ec8c4
Yan Pisani <54196956+yanpisani@users.noreply.github.com>
2021-11-08 18:06:16 -0300

    Sidebar criada (incompleta)

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/admin/login";
 import PassRecovery from "../pages/admin/pass_recovery";
+import Sidebar from "../components/admin/sidebar";
 
 export default function Router() {
   return (
@@ -9,6 +10,7 @@ export default function Router() {
       <Route path="/" exact element={<Login/>} />
       <Route path="/entrar" element={<Login/>} />
       <Route path="/recuperar-senha" element={<PassRecovery/>} />
+      <Route path="/sidebar" element={<Sidebar/>} />
       <Route render={() => <Navigate to={{ pathname: "/entrar" }} />} />
     </Routes>
   );
