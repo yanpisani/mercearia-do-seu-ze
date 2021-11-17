39aff7bd07c3d97915757da64db210da4fbf413a
jose.marques <jose.marques@gafit.com.br>
2021-11-17 15:22:14 -0300

    Arrumando rotas

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route} from "react-router-dom";
 import Login from "../pages/admin/login";
+import RecoveryPass from "../pages/admin/pass_recovery";
 import Layout from "../pages/admin/dashboard/";
 import Home from "../pages/admin/dashboard/home";
 import Stock from "../pages/admin/dashboard/stock";
@@ -9,7 +10,8 @@ export default function Router() {
   return (
       <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/entrar" element={<Login />} />
+          <Route path="/recuperar-senha" element={<RecoveryPass />} />
           <Route path="/admin" element={<Layout />}>
               <Route path="inicio" element={<Home />} />
               <Route path="estoque" element={<Stock />} />
