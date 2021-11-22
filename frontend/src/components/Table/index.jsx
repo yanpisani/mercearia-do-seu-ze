5fbdd3e8541dbe1d80ec6d54c2fb0c9668e92e93
Pedro Sampaio <pedrohenriquefsp90@gmail.com>
2021-11-22 15:43:53 -0300

    Added Table Component

---
+++ frontend/src/components/Table/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './style.scss';
+import eye from '../../assets/images/eye.png';
+import trash from '../../assets/images/trash.png';
+
+export default function TableComponent(props) {
+  function RenderTable() {
+    return (
+      <div className="all-table">
+        <table>
+          <thead>
+            <tr>
+              {props.data.header.map((item) => {
+                return <th>{item}</th>;
+              })}
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.data.data.map((row) => {
+              return (
+                <tr>
+                  {row.map((item) => {
+                    return <td>{item}</td>;
+                  })}
+                  <td className="icons">
+                    <img src={eye} />
+                    <img src={trash} />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  return <div>{RenderTable()}</div>;
+}
