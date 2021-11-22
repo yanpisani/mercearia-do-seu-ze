916491853caeb011b7bde1e6682ee0fd44d3c271
Pedro Sampaio <pedrohenriquefsp90@gmail.com>
2021-11-22 17:18:14 -0300

    Added get method

--- frontend/src/pages/admin/dashboard/stock/index.jsx
+++ frontend/src/pages/admin/dashboard/stock/index.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.scss';
 import TableComponent from '../../../../components/Table/index.jsx';
+import apiProduct from '../../../../services/api/product';
 
-export default function index() {
+export default function Stock() {
+  const [productData, setProductsData] = useState({
+    header: [],
+    data: [],
+  });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    getProducts();
+  }, []);
+
+  async function getProducts() {
+    const res = await apiProduct.getProducts();
+    let auxData = [];
+
+    for (let i = 0; i < res.length; i++) {
+      auxData[i] = [
+        res[i].name,
+        res[i].quantity,
+        res[i].cost_value,
+        res[i].sale_value,
+      ];
+    }
+    setProductsData(auxData);
+    setLoading(false);
+  }
+
   return (
     <div className="all-inventory">
       <div className="content-flex">
@@ -10,12 +38,14 @@ export default function index() {
           <b>Estoque</b>
         </h2>
         <br />
-        <TableComponent
-          data={{
-            header: ['Produto', 'Unidades', 'Valor Custo', 'Valor Venda'],
-            data: [],
-          }}
-        />
+        {!loading && (
+          <TableComponent
+            data={{
+              header: ['Produto', 'Unidades', 'Valor Custo', 'Valor Venda'],
+              data: productData,
+            }}
+          />
+        )}
       </div>
     </div>
   );
