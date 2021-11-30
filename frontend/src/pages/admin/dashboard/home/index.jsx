d181d3563b64ab83dd1b48882848c70300334b44
Pedro Sampaio <pedrohenriquefsp90@gmail.com>
2021-11-30 11:14:49 -0300

    Front-end stetic ajustment

--- frontend/src/pages/admin/dashboard/home/index.jsx
+++ frontend/src/pages/admin/dashboard/home/index.jsx
@@ -7,32 +7,40 @@ export default function Home() {
     {
       section: 'Funcionários',
       link: '/admin/funcionarios',
+      icon: 'ri-user-line',
     },
     {
       section: 'Estoque',
       link: '/admin/estoque',
+      icon: 'ri-home-8-line',
     },
     {
       section: 'Vendas',
       link: '/admin/sales',
+      icon: 'ri-shopping-cart-line',
     },
   ];
 
   return (
     <div className={`admin-home`}>
-      <h1>
-        <b>Área do Gerente/administrador</b>
-      </h1>
-      <div className={`admin-card`}>
-        {Cards.map((card, index) => {
-          return (
-            <Card
-              key={index.toString()}
-              section={card.section}
-              to={card.link}
-            />
-          );
-        })}
+      <div className="flex-center">
+        <h1>
+          <b>Área do Gerente/administrador</b>
+        </h1>
+      </div>
+      <div className="flex-center">
+        <div className={`admin-card`}>
+          {Cards.map((card, index) => {
+            return (
+              <Card
+                key={index.toString()}
+                section={card.section}
+                to={card.link}
+                icon={card.icon}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
