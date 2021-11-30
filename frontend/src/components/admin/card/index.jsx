7cd7811e7b84d60ede8360296cc7631dd06400d4
Pedro Sampaio <pedrohenriquefsp90@gmail.com>
2021-11-30 11:14:49 -0300

    Front-end stetic ajustment

--- frontend/src/components/admin/card/index.jsx
+++ frontend/src/components/admin/card/index.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import './styles.scss'
-import {Link} from "react-router-dom";
+import './styles.scss';
+import { Link } from 'react-router-dom';
 
 export default function Card(props) {
-    return(
-        <div className={`card-home`}>
-            <div className={`container`}>
-                <div className={`row`}>
-                    <div className={`col d-flex justify-content-center align-items-center card-title`}>{props.section}</div>
-                </div>
-                <div className={`row`}>
-                    <div className={`col d-flex justify-content-center align-items-center`}>
-                        <i className={`ri-user-line icon`} />
-                    </div>
-                </div>
-                <div className={`row`}>
-                    <Link className={`col d-flex justify-content-center align-items-center card-button`} to={props.to}>Editar</Link>
-                </div>
-            </div>
+  return (
+    <div className={`card-home`}>
+      <div className={`row`}>
+        <div
+          className={`col d-flex justify-content-center align-items-center card-title`}
+        >
+          <b>{props.section}</b>
         </div>
-    )
+      </div>
+      <div className={`row`}>
+        <div className={`col d-flex justify-content-center align-items-center`}>
+          <i className={`${props.icon} icon`} />
+        </div>
+      </div>
+      <div>
+        <Link
+          className={`col d-flex justify-content-center align-items-center card-button`}
+          to={props.to}
+        >
+          Editar
+        </Link>
+      </div>
+    </div>
+  );
 }
